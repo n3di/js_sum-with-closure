07cc235d5d84eb3edc5e1bb7694a388d382d3308
Michał Szwindowski <github@nedi.me>
2024-09-24 13:03:17 +0200

    Initial Commit

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,12 +1,25 @@
 'use strict';
 
 /**
- *
  * @return {function}
  */
-
 function makeRobotAccountant() {
-  // write code here
+  // Licznik wywołań, początkowo 0
+  let callCount = 0;
+
+  // Funkcja zwracana przez makeRobotAccountant
+  return function(a) {
+    return function(b) {
+      callCount++;
+
+      // Sprawdzamy, czy to czwarte lub kolejne wywołanie
+      if (callCount > 3 && callCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return a + b;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
